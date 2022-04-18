c939ff986895c4135fcc2427040813ff5ab7a255
Filipe de Deus <filipedeus2015@gmail.com>
2022-04-18 20:06:57 -0300

    Commit inicial

---
+++ backend/src/test/java/com/ceub/geduc/GeducApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceub.geduc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeducApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
