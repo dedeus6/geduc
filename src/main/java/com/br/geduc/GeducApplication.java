199fa56d4831e906969b3aef0a7e3056bccc6c6c
Filipe de Deus <filipedeus2015@gmail.com>
2022-09-08 19:56:22 -0300

    Fix mongodb connection

--- src/main/java/com/br/geduc/GeducApplication.java
+++ src/main/java/com/br/geduc/GeducApplication.java
@@ -2,8 +2,10 @@ package com.br.geduc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class GeducApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/br/geduc/repository/UserRepository.java
+++ src/main/java/com/br/geduc/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.br.geduc.repository;
 
 import com.br.geduc.document.UserDocument;
-import com.br.geduc.dto.request.UserRequestDTO;
 import com.br.geduc.dto.response.UserResponseDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
