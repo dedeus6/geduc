bf50c9383fa5f3c919466a44562ece2186a2f7a6
Filipe de Deus <filipedeus2015@gmail.com>
2022-10-24 14:07:18 -0300

    Adding cors

--- src/main/java/com/br/geduc/configuration/CorsConfiguration.java
+++ src/main/java/com/br/geduc/configuration/CorsConfiguration.java
@@ -10,8 +10,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://localhost:4200")
                 .allowedOrigins("https://geduc.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
