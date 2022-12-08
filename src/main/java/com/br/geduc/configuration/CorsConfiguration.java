922eaea499a58f755fd7a7a7bd6a2644bfdebc59
Filipe de Deus <filipedeus2015@gmail.com>
2022-12-08 09:25:32 -0300

    adding new cors

--- src/main/java/com/br/geduc/configuration/CorsConfiguration.java
+++ src/main/java/com/br/geduc/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.br.geduc.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +12,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://geduc.netlify.app/")
-                .allowedOrigins("http://localhost:4200/");
+                .allowedOrigins("http://localhost:4200/")
+                .allowedOrigins("*");
     }
 
 }
